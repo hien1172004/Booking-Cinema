788ddf465a3a43568fdb4196cc0074de8b2b2471
hien1172004 <hien1172004@gmail.com>
2026-04-19 16:26:34 +0700

    Fix CORS path for global access and Swagger UI

--- src/main/java/org/example/cinemaBooking/Config/SecurityConfig.java
+++ src/main/java/org/example/cinemaBooking/Config/SecurityConfig.java
@@ -145,7 +145,7 @@ public class SecurityConfig {
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
     @Bean
